f896d20e9d330158c59c9cf45c73d4aa3076acd3
yutarox1 <yutaro@DESKTOP-BHOI1Q2.localdomain>
2021-11-07 20:16:29 +0900

    first commit

---
+++ boardapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc, BoardCleate
+
+urlpatterns = [
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+    path('list/', listfunc, name='list' ),
+    path('logout/', logoutfunc, name='logout'),
+    path('detail/<int:pk>', detailfunc, name='detail'),
+    path('good/<int:pk>', goodfunc, name='good'),
+    path('read/<int:pk>', readfunc, name='read'),
+    path('create/', BoardCleate.as_view(), name='create')
+]
+
+
+
